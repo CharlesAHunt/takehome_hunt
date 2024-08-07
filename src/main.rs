0091f469ff3483ff31f557f588f3a4bf2d514834
Charles Hunt <charlesalberthunt@gmail.com>
2024-08-07 13:42:49 -0700

    wrap up

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,17 @@
 mod models;
 
-use std::collections::HashMap;
-use std::process::exit;
+use crate::models::DataStore;
 use chrono::prelude::*;
 use reqwest::Error;
+use std::collections::HashMap;
+use std::process::exit;
 use std::time::Duration;
-use crate::models::DataStore;
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
-
     let initial_data = DataStore {
-        price_history:HashMap::new(),
-        stats:HashMap::new()
+        price_history: HashMap::new(),
+        stats: HashMap::new(),
     };
 
     let delay = Duration::from_millis(1000);
@@ -28,7 +27,7 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
             // let stats_debug = _data_store.stats;
             // println!("Stats Results: {stats_debug:#?}"); //Uncomment to see all the stats data for each currency
             exit(0)
-        },
+        }
         Err(error) => {
             let alert = models::Alert {
                 timestamp: Utc::now(),
@@ -41,20 +40,17 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
                     avg_price: None,
                     deviation: None,
                     price_change: None,
-                }
+                },
             };
             println!("{:?}", alert);
             exit(1);
         }
     }
-
 }
 
-async fn monitor_currency_pairs(mut ds: DataStore)-> Result<DataStore, Error> {
+async fn monitor_currency_pairs(mut ds: DataStore) -> Result<DataStore, Error> {
     let resp = match reqwest::get("https://api.gemini.com/v1/pricefeed").await {
-        Ok(response) => {
-            response
-        }
+        Ok(response) => response,
         Err(error) => {
             let alert = models::Alert {
                 timestamp: Utc::now(),
@@ -67,31 +63,33 @@ async fn monitor_currency_pairs(mut ds: DataStore)-> Result<DataStore, Error> {
                     avg_price: None,
                     deviation: None,
                     price_change: None,
-                }
+                },
             };
             println!("{:?}", alert);
             exit(1);
         }
-    }.json::<Vec<models::SymbolPricePairAPI>>().await?;
+    }
+    .json::<Vec<models::SymbolPricePairAPI>>()
+    .await?;
 
     let resp_iterator = resp.into_iter();
 
     for val in resp_iterator {
-        let latest_price:f32 = val.price.parse().unwrap();
+        let latest_price: f32 = val.price.parse().unwrap();
         let pair_key = &val.pair;
         let pair_stat = match ds.stats.get(pair_key) {
             None => {
                 let price_history = match ds.price_history.get(pair_key) {
                     None => vec![latest_price],
-                    Some(history) => history.to_owned()
+                    Some(history) => history.to_owned(),
                 };
                 ds.price_history.insert(pair_key.to_owned(), price_history);
-                models::PairStats{
+                models::PairStats {
                     std_dev: 0.0,
                     mean: latest_price,
-                    most_recent_price: latest_price
+                    most_recent_price: latest_price,
                 }
-            },
+            }
             Some(stats) => {
                 if (latest_price - stats.mean) > stats.std_dev {
                     let alert = models::Alert {
@@ -103,23 +101,29 @@ async fn monitor_currency_pairs(mut ds: DataStore)-> Result<DataStore, Error> {
                             error_message: None,
                             last_price: Option::from(latest_price),
                             avg_price: Option::from(stats.mean),
-                            deviation: Option::from(if stats.std_dev == 0.0 { stats.std_dev } else { latest_price / stats.std_dev }),
-                            price_change: Option::from(stats.most_recent_price - latest_price)
-                        }
+                            deviation: Option::from(if stats.std_dev == 0.0 {
+                                stats.std_dev
+                            } else {
+                                latest_price / stats.std_dev
+                            }),
+                            price_change: Option::from(stats.most_recent_price - latest_price),
+                        },
                     };
                     println!("{:?}", sonic_rs::to_string(&alert).unwrap());
                 };
                 let price_history = match ds.price_history.get(pair_key) {
                     None => vec![latest_price],
-                    Some(history) =>  [history.to_owned(), vec![latest_price]].concat()
+                    Some(history) => [history.to_owned(), vec![latest_price]].concat(),
                 };
-                ds.price_history.insert(pair_key.to_owned(), price_history.to_owned());
+                ds.price_history
+                    .insert(pair_key.to_owned(), price_history.to_owned());
                 let new_mean = statistical::mean(&price_history);
-                let new_std_dev = statistical::standard_deviation(&price_history, Option::from(new_mean));
-                models::PairStats{
+                let new_std_dev =
+                    statistical::standard_deviation(&price_history, Option::from(new_mean));
+                models::PairStats {
                     std_dev: new_std_dev,
                     mean: new_mean,
-                    most_recent_price: latest_price
+                    most_recent_price: latest_price,
                 }
             }
         };
--- src/models.rs
+++ src/models.rs
@@ -1,17 +1,16 @@
-
 use sonic_rs::{Deserialize, Serialize};
 use std::collections::HashMap;
 
 #[derive(Debug)]
 pub(crate) struct PairStats {
-    pub std_dev:f32,
+    pub std_dev: f32,
     pub mean: f32,
-    pub most_recent_price: f32
+    pub most_recent_price: f32,
 }
 
 pub(crate) struct DataStore {
     pub price_history: HashMap<String, Vec<f32>>,
-    pub stats: HashMap<String, PairStats>
+    pub stats: HashMap<String, PairStats>,
 }
 
 #[derive(Serialize, Deserialize, Debug)]
@@ -28,7 +27,7 @@ pub(crate) struct Alert {
     pub log_level: String,
     pub trading_pair: Option<String>,
     pub is_deviation: bool,
-    pub data: AlertMetaData
+    pub data: AlertMetaData,
 }
 
 #[derive(Serialize, Deserialize, Debug)]
@@ -37,5 +36,5 @@ pub(crate) struct AlertMetaData {
     pub last_price: Option<f32>,
     pub avg_price: Option<f32>,
     pub deviation: Option<f32>,
-    pub price_change: Option<f32>
+    pub price_change: Option<f32>,
 }
